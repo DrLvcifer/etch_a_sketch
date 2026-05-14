0f022a476367558459ffb07cf523451202fc03b2
DrLvcifer <DrLvcifer@gmail.com>
2026-05-14 12:54:57 +0100

    feature: added custom resolution selection | UI: added brush cursor on the sketch screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,12 @@
-/*______________
-*  SCREEN LOGIC
---------------*/
-const container = document.getElementById("flex-container");
-
-for (let i = 0; i < 256; i++) {
-   const pixel = document.createElement("div");
-   pixel.classList.add("pixel");
-   container.appendChild(pixel);
-}
-
-container.addEventListener("mouseover", (e) => {
-   if (e.target.classList.contains("pixel")) {
-      e.target.classList.add("is-active");
-   }
-});
-
 /*________________
 *  SETTINGS EVENTS
 ----------------*/
 
 const dialog = document.getElementById("pop-up");
 const setBtn = document.getElementById("settings-btn");
-const confirmBtn = document.getElementById("confirm-btn");
 const cancelBtn = document.getElementById("cancel-btn");
+const confirmBtn = document.getElementById("confirm-btn");
+const container = document.getElementById("flex-container");
 
 setBtn.addEventListener("click", () => {
    dialog.showModal();
@@ -32,4 +16,38 @@ cancelBtn.addEventListener("click", () => {
    dialog.close("cancel");
 });
 
-confirmBtn.addEventListener("click", (e) => {});
+/*________________________
+*  SCREEN BRAIN & LOGIC
+------------------------*/
+function createSketchGrid(sketchResolution) {
+   sketchSize = Math.sqrt(sketchResolution);
+   pixelSize = 100 / sketchSize;
+
+   for (let i = 0; i < sketchResolution; i++) {
+      const pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.style.width = `${pixelSize}%`;
+      pixel.style.height = `${pixelSize}%`;
+      container.appendChild(pixel);
+   }
+
+   container.addEventListener("mouseover", (e) => {
+      if (e.target.classList.contains("pixel")) {
+         e.target.classList.add("is-active");
+      }
+   });
+}
+
+createSketchGrid(256);
+
+confirmBtn.addEventListener("click", () => {
+   const selectedResolution = document.querySelector(
+      'input[name="resolution"]:checked'
+   );
+
+   if (selectedResolution) {
+      container.innerHTML = "";
+      const sketchResolution = parseInt(selectedResolution.value);
+      createSketchGrid(sketchResolution);
+   }
+});
